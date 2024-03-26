558799ed2d4d2ba89672fa8502ab319edf0652f4
ali-jin <alina.jin.contact@gmail.com>
2024-03-26 13:29:40 +0100

    7-timeout.js

---
+++ Javascript_advanced/7-timeout.js
@@ -0,0 +1,10 @@
+console.log("Start of the execution queue");
+setTimeout(() => {
+    console.log("Final code block to be executed")
+}, 0);
+
+for (let index = 0; index < 100; index++) {
+    console.log(index + 1);
+}
+
+console.log("End of the loop printing");
