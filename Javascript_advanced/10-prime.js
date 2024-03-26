77ce5ad53ffd77927970393d62509c15680c98fc
ali-jin <alina.jin.contact@gmail.com>
2024-03-26 14:17:20 +0100

    10-prime.js

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,45 @@
+/**
+ * Check if the number is prime.
+ * @param {number} number - the number to check.
+ * @returns {boolean} - return true or false.
+ */
+function isPrime(number) {
+    let count = 0;
+
+    for (let i = 1; i <= number; i++) {
+        if (number % i == 0) {
+            count = count + 1;
+        }
+    }
+    if (count == 2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+/**
+ * Count the number of prime numbers between 2 and 100.
+ * @returns {number} - number of prime numbers.
+ */
+function countPrimeNumbers() {
+    let count = 0;
+
+    for (let index = 2; index <= 100; index++) {
+        if (isPrime(index)) {
+            count++;
+        };
+    }
+
+    return count;
+}
+
+let startTime = performance.now();
+
+for (let index = 0; index < 100; index++) {
+    countPrimeNumbers();
+}
+
+let endTime = performance.now();
+console.log(`Execution time of calculating prime numbers 100 times was ${endTime - startTime} milliseconds.`);
